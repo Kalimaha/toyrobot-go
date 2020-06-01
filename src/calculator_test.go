20971b17337d11d5df7bbb0d0ae98ee86b8c6b58
Kalimaha <guido.barbaglia@gmail.com>
2020-06-01 10:20:12 +1000

    First test

---
+++ src/calculator_test.go
@@ -0,0 +1,26 @@
+package main
+
+import(
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"testing"
+)
+
+func TestCalculator(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Calculator Suite")
+}
+
+var _ = Describe("Sum", func() {
+	var a int
+	var b int
+
+	BeforeEach(func() {
+		a = 2
+		b = 3
+	})
+	
+	It("sums two numbers", func() {
+		Expect(Sum(a, b)).To(Equal(5))
+	})
+})
